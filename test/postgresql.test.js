068e4a8976b86830922a22c7b31f3f085456e5e3
Raymond Feng <raymond@strongloop.com>
2014-05-02 13:32:51 -0700

    Fix boolean data mapping
    See https://github.com/strongloop/loopback-connector-postgresql/issues/2

--- test/postgresql.test.js
+++ test/postgresql.test.js
@@ -1,6 +1,53 @@
 require('loopback-datasource-juggler/test/common.batch.js');
 require('loopback-datasource-juggler/test/include.test.js');
 
+
+var should = require('should');
+
+var Post, db;
+
+describe('postgresql connector', function () {
+
+  before(function () {
+    db = getDataSource();
+
+    Post = db.define('PostWithBoolean', {
+      title: { type: String, length: 255, index: true },
+      content: { type: String },
+      approved: Boolean
+    });
+  });
+
+  it('should run migration', function (done) {
+    db.automigrate('PostWithBoolean', function () {
+      done();
+    });
+  });
+
+  it('should support boolean types with true value', function(done) {
+    Post.create({title: 'T1', content: 'C1', approved: true}, function(err, p) {
+      should.not.exists(err);
+      Post.findById(p.id, function(err, p) {
+        should.not.exists(err);
+        p.should.have.property('approved', true);
+        done();
+      });
+    });
+  });
+
+  it('should support boolean types with false value', function(done) {
+    Post.create({title: 'T1', content: 'C1', approved: false}, function(err, p) {
+      should.not.exists(err);
+      Post.findById(p.id, function(err, p) {
+        should.not.exists(err);
+        p.should.have.property('approved', false);
+        done();
+      });
+    });
+  });
+
+});
+
 // FIXME: The following test cases are to be reactivated for PostgreSQL
 /*
 
