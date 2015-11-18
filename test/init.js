7031093c67779f0ede4f003333b2e64ad9133b35
cgole <chandrika@strongloop.com>
2015-11-18 14:34:03 -0800

    add CI DB server

--- test/init.js
+++ test/init.js
@@ -2,6 +2,16 @@ var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = require('rc')('loopback', {test: {postgresql: {}}}).test.postgresql;
 
+if (process.env.CI) {
+  config = {
+    host: process.env.POSTGRESQL_HOST || config.host || 'localhost',
+    port: process.env.POSTGRESQL_PORT || config.port || 5432,
+    database: 'test',
+    username: process.env.POSTGRESQL_USERNAME || config.username,
+    password: process.env.POSTGRESQL_PASSWORD || config.password
+  };
+}
+
 global.getDataSource = global.getSchema = function () {
   var db = new DataSource(require('../'), config);
   db.log = function (a) {
