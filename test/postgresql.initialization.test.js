0fd1ff22386095ee115424f0ff81f13aaa645d74
gregdingle <gregdingle@gmail.com>
2016-11-15 22:55:28 +0100

    Added test. Ran run-tests.

---
+++ test/postgresql.initialization.test.js
@@ -0,0 +1,52 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-connector-postgresql
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+var connector = require('..');
+var DataSource = require('loopback-datasource-juggler').DataSource;
+var should = require('should');
+
+describe('initialization', function() {
+  it('honours user-defined pg-pool settings', function(done) {
+    var dataSource = new DataSource(connector, {});
+    var pool = dataSource.connector.pg.pool;
+    pool._factory.max.should.not.equal(999);
+
+    var settings = {max: 999}; // non-default value
+    var dataSource = new DataSource(connector, settings);
+    var pool = dataSource.connector.pg.pool;
+    pool._factory.max.should.equal(999);
+
+    done();
+  });
+
+  it('honours user-defined url settings', function(done) {
+    var settings = {url: 'postgres://'};
+
+    var dataSource = new DataSource(connector, {});
+    var clientConfig = dataSource.connector.clientConfig;
+    should.not.exist(clientConfig.connectionString);
+
+    var dataSource = new DataSource(connector, settings);
+    var clientConfig = dataSource.connector.clientConfig;
+    clientConfig.connectionString.should.equal(settings.url);
+
+    done();
+  });
+
+  it('honours multiple user-defined settings', function(done) {
+    var settings = {url: 'postgres://', max: 999};
+
+    var dataSource = new DataSource(connector, settings);
+    var pool = dataSource.connector.pg.pool;
+    pool._factory.max.should.equal(999);
+
+    var clientConfig = dataSource.connector.clientConfig;
+    clientConfig.connectionString.should.equal(settings.url);
+
+    done();
+  });
+});
