16a7eda0b3777a9e18ee81d40ea5fb85cb04c98c
Loay <loay@users.noreply.github.com>
2017-04-25 16:27:17 -0400

    Add connector specific feature

--- test/postgresql.test.js
+++ test/postgresql.test.js
@@ -132,12 +132,12 @@ describe('postgresql connector', function() {
   });
 
   it('should support order with random sorting', function(done) {
-    Post.find({order: 'rand'}, function(err, randomPosts1) {
+    Post.find({order: '${random}'}, function(err, randomPosts1) {
       should.not.exists(err);
       var order1 = randomPosts1.map(function(u) { return u.id; });
       (order1.length).should.eql(randomPosts1.length);
       order1.should.containEql(1, 2, 3);
-      Post.find({order: 'rand'}, function(err, randomPosts2) {
+      Post.find({order: '${random}'}, function(err, randomPosts2) {
         should.not.exist(err);
         var order2 = randomPosts2.map(function(u) { return u.id; });
         (order2.length).should.eql(randomPosts2.length);
