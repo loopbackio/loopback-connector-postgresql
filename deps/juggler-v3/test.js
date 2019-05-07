bc9c6005a1b624e694ef227953d9af266ed446d4
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 13:34:12 -0400

    chore: update copyrights years

--- deps/juggler-v3/test.js
+++ deps/juggler-v3/test.js
@@ -1,7 +1,7 @@
-// Copyright IBM Corp. 2013,2016. All Rights Reserved.
-// Node module: loopback-connector-mysql
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: loopback-connector-postgresql
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
 
 'use strict';
 
